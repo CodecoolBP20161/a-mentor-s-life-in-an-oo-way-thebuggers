d3a80a50a5969a174f4b4c5b588bc9554a1547e1
b-franka <b.franku@gmail.com>
2016-06-28 14:54:25 +0200

    added lunch.py

---
+++ lunch.py
@@ -0,0 +1,6 @@
+class Food():
+
+    def __init__(name, vegetarian, restaurant_status):
+        self.name = name
+        self.vegetarian = vegetarian
+        self.restaurant_status = restaurant_status
