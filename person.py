75e0867245e8a24f2f9f389f88d6286f5e65c370
Kincses Dani <danikincs@gmail.com>
2016-06-29 11:38:04 +0200

    Merge problem

--- person.py
+++ person.py
@@ -1,7 +1,7 @@
 class Person:
     def __init__(self, first_name, last_name, year_of_birth, gender, energy_level, vegan, hungriness):
         self.first_name = first_name
-        self. last_name = last_name
+        self.last_name = last_name
         self.year_of_birth = year_of_birth
         self.gender = gender
         self.energy_level = energy_level
@@ -10,5 +10,4 @@ class Person:
 
     def eat(self, food_object):
         if self.vegan:
-            if energy_level kisfaszom:
-                if food_object.vegan:
+            pass
