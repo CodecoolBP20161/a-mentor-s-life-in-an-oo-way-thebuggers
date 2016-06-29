d140aeedaf077655899db6e7ecf3fcf2d383e1c5
Kincses Dani <danikincs@gmail.com>
2016-06-29 12:05:55 +0200

    merge problem

--- robber.py
+++ robber.py
@@ -6,4 +6,4 @@ class Robber(Person):
     def __init__(self, outfit, weapon, *args, **kwargs):
         self.outfit = outfit
         self.weapon = weapon
-        super.(Robber, self).__init__(*args, **kwargs)
+        super(Robber, self).__init__(*args, **kwargs)
--- story.py
+++ story.py
@@ -1,5 +1,6 @@
 from codecool_class import CodecoolClass
 from mentor import Mentor
 from student import Student
+from pornstar import Pornstar
 
 codecool_bp = CodecoolClass.create_local
