969742b6712d0d199eb74ca67d9edcad1a242d75
b-franka <b.franku@gmail.com>
2016-06-29 12:14:22 +0200

    generate local added

--- codecool_class.py
+++ codecool_class.py
@@ -3,4 +3,20 @@ from student import Student
 
 
 class CodecoolClass:
-    pass
+
+    def __init__(self, location, year, mentors, students):
+        self.location = location
+        self.year = year
+        self.mentors = mentors
+        self.students = students
+
+    @classmethod
+    def generate_local(cls):
+        cls.location = "Budapest"
+        cls.year = 2016
+        cls.mentors = Mentor.create_by_csv(cls, 'data/mentors.csv')
+        cls.students = "sanyi"
+        return CodecoolClass(cls.location, cls.year, cls.mentors, cls.students)
+
+    def find_student_by_full_name(self, full_name):
+        mentor_name = ("%s %s" % (self.mentors[0], self.mentors[1]))
