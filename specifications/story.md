f3b8eee30bc26ecaad471619ed89973736ef6fce
b-franka <b.franku@gmail.com>
2016-06-30 13:13:55 +0200

    continue story with lunchbreak

--- specifications/story.md
+++ specifications/story.md
@@ -6,7 +6,7 @@
 
 * after that the door bell rings and Miki mentor opens the door. Miki's energy level is high so when he sees that a pornstar is at the door, he goes to the other office with her and does a casting.
 
-* the next event is launchbreak at noon. since Tomi mentor's hungry level is at the top, he goes for a big bowl of chicken despite the fact he's a vegan.
+* the next event is lunchbreak at noon. since Tomi mentor's hungry level is at the top, he goes for a big bowl of chicken despite the fact he's a vegan.
 
 * we see that owen wants to eat his good 'ol regular chicken, but its closed, so he dies of hunger, and gets deleted from the list of students.
 
--- story.py
+++ story.py
@@ -2,24 +2,64 @@ from codecool_class import CodecoolClass
 from mentor import Mentor
 from student import Student
 from pornstar import Pornstar
+from lunch import Food
 
-codecool_bp = CodecoolClass.generate_local()
-print("School @ {0}, in year {1} is created, with {2} mentors and 58 students".format(codecool_bp.location, codecool_bp.year, len(codecool_bp.mentors)))
-mentor_tomi = codecool_bp.find_mentor_by_full_name("Tamás Tompa")
-print("{0} {1} was found between the mentors with {2} energy".format(mentor_tomi.first_name, mentor_tomi.last_name, mentor_tomi.energy_level))
-mentor_miki = codecool_bp.find_mentor_by_full_name("Miklós Beöthy")
-print("{0} {1} was found between the mentors with {2} energy".format(mentor_miki.first_name, mentor_miki.last_name, mentor_miki.energy_level))
-mentor_dani = codecool_bp.find_mentor_by_full_name("Dániel Salamon")
-print("{0} {1} was found between the mentors with {2} energy".format(mentor_dani.first_name, mentor_dani.last_name, mentor_dani.energy_level))
-student_franka = codecool_bp.find_student_by_full_name(codecool_bp.get_random_student())
-student_gyuri = codecool_bp.find_student_by_full_name(codecool_bp.get_random_student())
-student_owen = codecool_bp.find_student_by_full_name(codecool_bp.get_random_student())
-student_dani = codecool_bp.find_student_by_full_name(codecool_bp.get_random_student())
-student_miki = codecool_bp.find_student_by_full_name(codecool_bp.get_random_student())
-print("Student {0} energy level is high in the morning".format(student_franka.first_name))
-mentor_dani.morning_routine(student_franka)
-print("Student {0} energy level is low in the morning".format(student_gyuri.first_name))
-mentor_miki.morning_routine(student_gyuri)
-print("Someone ring the doorbell")
-pornstar = Pornstar("Crystal Ray", "One big black Dildo")
-mentor_miki.open_ringing_door(pornstar)
+
+def print_find_mentor(someone):
+    print("{0} {1} was found between the mentors with {2} energy".format(
+        someone.first_name, someone.last_name, someone.energy_level))
+
+
+def init_school():
+    # create school
+    codecool_bp = CodecoolClass.generate_local()
+    print("School @ {0}, in year {1} is created, with {2} mentors and 58 students".format(
+        codecool_bp.location, codecool_bp.year, len(codecool_bp.mentors)))
+    # create mentors
+    mentor_tomi = codecool_bp.find_mentor_by_full_name("Tamás Tompa")
+    print_find_mentor(mentor_tomi)
+    mentor_miki = codecool_bp.find_mentor_by_full_name("Miklós Beöthy")
+    print_find_mentor(mentor_miki)
+    mentor_dani = codecool_bp.find_mentor_by_full_name("Dániel Salamon")
+    print_find_mentor(mentor_dani)
+    teachers = [mentor_miki, mentor_tomi, mentor_dani]
+    # create list of 5 students
+    students = []
+    for i in range(5):
+        students.append(codecool_bp.get_random_student())
+    return [students, teachers]
+
+
+def morning_routine(school):
+    print("Student {0} energy level is high in the morning".format(school[0][0]))
+    school[1][2].morning_routine(school[0][0])
+    print("Student {0} energy level is low in the morning".format(school[0][1]))
+    school[1][0].morning_routine(school[0][1])
+
+
+def door_ringing(opener, ringer):
+    opener.open_ringing_door(ringer)
+
+
+def lunchbreak(school):
+    steak = Food("chicken steak", False, True)
+    school[1][1].eat(steak)
+    chichken = Food("chichken @csirkés", False, False)
+    school[0][2].eat(chichken)
+
+
+def main():
+    school = init_school()
+    init_school()
+    morning_routine(school)
+    pornstar = Pornstar("Crystal Ray", "One big black Dildo")
+    opener = school[1][0]
+    door_ringing(opener, pornstar)
+    lunchbreak(school)
+    robber = Robber("Viszkis", "Robbermask", "Bottle")
+    opener = school[1][1]
+    door_ringing(opener, robber)
+
+
+
+main()
