e950bf817dd3f1c6f3d20da4d896933bf49d54fd
ernstoliver <ernstoliver94@gmail.com>
2016-06-30 10:45:41 +0200

    added more story

--- student.py
+++ student.py
@@ -20,10 +20,15 @@ class Student(Person):
 
     def ending_routine(self, mentor):
         if self.energy_level == 1:
-            print("alcohol")
-        if self.energy_level == 2:
-            print("weed")
+            print("""Our great mentor is low on energy and his mood lacks of happiness, so he decided to make things
+            a little more exciting by announcing a self evaluation opportunity to the students, and by self evaluation,
+            he meant a mandatory exam. He thought that would be nice towards codecoolers if the eligible level would be
+            set to 98%. /"Im such a sweetheart/" he said, as he was astonished by his own generosity""")
         if self.energy_level == 3:
-            print("cocain")
+            print("""Our great mentor felt a great amount of energy in himself, /"Its time to give/" he thought.
+            So he decided to buy beer for the entire class, he later realized the consequences of his generosity as his
+            purse became way more lighter than before. But this was a day to give, not to take.""")
         else:
-            print("fuck off")
+            print("""Our great mentor has reached the proper energy level, which made him realize the fact that no one gives
+            a flying fuck about codecool. /"My work is done here/" thats what he said to himself while he was
+            walking out of the office building, located under Nagymezo street 44""")
