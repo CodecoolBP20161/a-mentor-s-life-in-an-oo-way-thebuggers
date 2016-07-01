0e8700551fc305ee4461c6c0499ba87326315a7d
b-franka <b.franku@gmail.com>
2016-07-01 09:22:27 +0200

    changed ending routine

--- student.py
+++ student.py
@@ -7,10 +7,9 @@ class Student(Person):
         super(Student, self).__init__(*args, **kwargs)
         self.knowledge_level = knowledge_level
 
-
     @classmethod
     def create_by_csv(cls, csv_file):
-        print("Mentors are initialized from CSV")
+        print("Students are initialized from CSV")
         cls.students = []
         with open(csv_file, newline='') as csvfile:
             all_students = csv.reader(csvfile)
@@ -20,16 +19,19 @@ class Student(Person):
         return cls.students
 
     def ending_routine(self, mentor):
-        if self.energy_level == 1:
-            print("""Our great mentor is low on energy and his mood lacks of happiness, so he decided to make things
-            a little more exciting by announcing a self evaluation opportunity to the students, and by self evaluation,
-            he meant a mandatory exam. He thought that would be nice towards codecoolers if the eligible level would be
-            set to 98%. /"Im such a sweetheart/" he said, as he was astonished by his own generosity""")
-        if self.energy_level == 3:
-            print("""Our great mentor felt a great amount of energy in himself, /"Its time to give/" he thought.
-            So he decided to buy beer for the entire class, he later realized the consequences of his generosity as his
-            purse became way more lighter than before. But this was a day to give, not to take.""")
+        if int(mentor.energy_level) == 1:
+            print("""
+Our great mentor is low on energy and his mood lacks of happiness, so he decided to make things
+a little more exciting by announcing a self evaluation opportunity to the students, and by self evaluation,
+he meant a mandatory exam. He thought that would be nice towards codecoolers if the eligible level would be
+set to 98%. /"Im such a sweetheart/" he said, as he was astonished by his own generosity""")
+        elif int(mentor.energy_level) == 3:
+            print("""
+Our great mentor felt a great amount of energy in himself, /"Its time to give/" he thought.
+So he decided to buy beer for the entire class, he later realized the consequences of his generosity as his
+purse became way more lighter than before. But this was a day to give, not to take.""")
         else:
-            print("""Our great mentor has reached the proper energy level, which made him realize the fact that no one gives
-            a flying fuck about codecool. /"My work is done here/" thats what he said to himself while he was
-            walking out of the office building, located under Nagymezo street 44""")
+            print("""
+Our great mentor has reached the proper energy level, which made him realize the fact that no one gives
+a flying fuck about codecool. /"My work is done here/" thats what he said to himself while he was
+walking out of the office building, located under Nagymezo street 44""")
